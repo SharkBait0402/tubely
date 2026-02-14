9a2812398c86d52647d5e17219baba9abbef9021
Casen Nagy <casen.nagy@gmail.com>
2026-02-13 19:01:17 -0600

    did almost nothing tbh... deadlock time

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -4,4 +4,6 @@ import (
 	"net/http"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	const maxMemory:=http.MaxBytesReader(w, r, 1 << 30)
+}
